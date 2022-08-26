059548a180539fb0f8bc102444e3f5ab47dca943
npatel99 <50912904+npatel99@users.noreply.github.com>
2022-08-26 21:02:47 +0530

    Hello and MySIrG in different line

---
+++ prog_2.c
@@ -0,0 +1,14 @@
+/*Write a C++ program to print Hello on the first line and MySirG on the second line
+using endl.*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout <<"Hello"<< endl;
+    cout <<"MySirG"<< endl;
+    
+    return 0;
+}
