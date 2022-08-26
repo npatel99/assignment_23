d9be2911ebde28e38bb76bd2e73ceb5c7707854f
npatel99 <50912904+npatel99@users.noreply.github.com>
2022-08-26 21:20:58 +0530

    square of number

---
+++ prog_7.c
@@ -0,0 +1,18 @@
+/*Write a C++ program to calculate the square of a number
+square = n*n */
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    
+    cout<<"Enter numbers: ";
+    cin >> n;
+    
+    cout <<"square of " << n << " is: "<< n*n << endl;
+    
+    return 0;
+}
