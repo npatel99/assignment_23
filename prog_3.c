c2e73512eac3854f642627f8aeb08fb748eb9df2
npatel99 <50912904+npatel99@users.noreply.github.com>
2022-08-26 21:05:40 +0530

    sum a+b

---
+++ prog_3.c
@@ -0,0 +1,16 @@
+/*Write a C++ program to calculate the sum of two numbers.*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout<<"Enter two number: ";
+    cin >> a >> b;
+    
+    cout <<"sum of both number is: "<< a+b << endl;
+    
+    return 0;
+}
